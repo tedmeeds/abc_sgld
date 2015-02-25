b243bd1ee0ba4a4b531be0312de3279313d3a2cf
Ted Meeds <tmeeds@gmail.com>
2015-02-25 13:32:17 +0100

    added sold to working code

--- code/working_code.py
+++ code/working_code.py
@@ -149,6 +149,119 @@ def run_mcmc( problem, params, theta, x = None ):
       print "t = %04d    loglik = %3.3f    theta = %3.3f    x = %3.3f"%(t+1,loglike_x,theta,x)
   return np.squeeze(np.array( THETAS)), np.squeeze(np.array(X)), np.squeeze(np.array(LL)), np.squeeze(np.array(OMEGAS))
 
+def run_sgld( problem, params, theta, x = None ):
+  
+  T             = params["T"]
+  S             = params["S"]
+  verbose_rate  = params["verbose_rate"]
+  omega_params  = params["omega_params"]
+  eta           = params["eta"] # Hamiltonain step size
+  d_theta       = params["d_theta"] # step used for estimating gradients
+  grad_U_func   = params["grad_func"]
+  grad_U_params = params["grad_params"]
+  
+  keep_x        = params["keep_x"] # the HABC methods do not necessarily have x in the state space
+  
+  # keep theta within bounds
+  lower_bounds = params["lower_bounds"]
+  upper_bounds = params["upper_bounds"]
+  
+  D   = len(theta)
+  
+  outputs = {}
+  # two_gradients = []
+  # one_gradients = []
+  # true_gradients = []
+  
+  omega = init_omega( omega_params )
+  
+  # pseudo-data
+  if keep_x:
+    if x is None:
+      x = problem.simulate( theta, omega, S )
+    assert len(x.shape) == 2, "x should be S by dim"
+    loglike_x = problem.loglike_x( x )
+    X      = [x]
+    LL     = [loglike_x]
+  else:
+    x         = None
+    loglike_x = None
+    
+  
+  
+  THETAS = [theta]  
+  OMEGAS = [omega]
+  
+  grads = np.zeros( (T,D) )
+  
+  # sample...
+  for t in xrange(T):
+    
+    # ----------------------------- # 
+    # simulate trajectory for theta #
+    # ----------------------------- #
+    
+    # estimate stochastic gradient
+    grad_U = grad_U_func( theta, d_theta, omega, S, grad_U_params)
+    #
+    if grad_U_params["record_2side_sl_grad"]:
+      grad_U_dummy = problem.two_sided_sl_gradient( theta, d_theta, omega, S, grad_U_params )
+    if grad_U_params["record_2side_keps_grad"]:
+      grad_U_dummy = problem.two_sided_keps_gradient( theta, d_theta, omega, S, grad_U_params )
+    if grad_U_params["record_true_abc_grad"]:
+      grad_U_dummy = problem.true_abc_gradient( theta, d_theta, S, grad_U_params )
+    if grad_U_params["record_true_grad"]:
+      grad_U_dummy = problem.true_gradient( theta, grad_U_params )
+    
+    grads[t,:] = grad_U
+    
+    # initialize momentum  
+    p = np.random.randn(D)
+    
+    # take step momentum
+    p = p - grad_U*eta/2.0
+    
+    # full step position
+    theta = theta + p*eta
+    
+    # bounce position off parameter boundaries
+    theta, p = bounce_off_boundaries( theta, p, lower_bounds, upper_bounds )
+    
+    # --------------- #
+    # samples omegas  #
+    # --------------- #
+    if omega_params["use_omega"] and np.random.rand() < omega_params["omega_rate"]:
+      if omega_params["omega_sample"]:
+        # propose new omega and accept/reject using MH
+        theta, x, omega, loglike_x = omega_sample(problem, theta, x, omega, loglike_x )
+      elif omega_params["omega_switch"]:
+        # randomly switch to new omega
+        omega = init_omega( omega_params )
+
+    if keep_x:
+      x = problem.simulate( theta, omega )
+      loglike_x = problem.loglike_x( x )
+      LL.append(loglike_x)
+      X.append(x)
+    
+    THETAS.append(theta)
+    OMEGAS.append(omega)
+    
+    
+    if np.mod(t+1,verbose_rate)==0:
+      if keep_x:
+        print "t = %04d    loglik = %3.3f    theta0 = %3.3f    x0 = %3.3f"%(t+1,loglike_x,theta[0],x[0])
+      else:
+        print "t = %04d    theta0 = %3.3f"%(t+1,theta[0])
+      
+  outputs["THETA"] = np.squeeze(np.array( THETAS))
+  outputs["OMEGA"] = np.squeeze(np.array(OMEGAS))
+  outputs["GLOGS"] = grad_U_params["logs"]
+  if keep_x:
+    outputs["X"] = np.squeeze(np.array(X))
+    outputs["LL"] = np.squeeze(np.array(LL))
+  return outputs
+  
 def run_sghmc( problem, params, theta, x = None ):
   
   T             = params["T"]

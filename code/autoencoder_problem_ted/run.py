212cba456a9694940b31c3de57d8051b0e0d83cc
Ted Meeds <tmeeds@gmail.com>
2015-03-01 17:16:52 +0100

    exp results

--- code/autoencoder_problem_ted/run.py
+++ code/autoencoder_problem_ted/run.py
@@ -12,14 +12,14 @@ from nn import *
 
 keep_x        = True
 init_seed     = 1
-T             = 5000 # nbr of samples
+T             = 50000 # nbr of samples
 verbose_rate  = 10
-C             = 20.01    # injected noise variance parameter
-eta           = 0.001 # step size for Hamiltoniam dynamics
+C             = 600.01    # injected noise variance parameter
+eta           = 0.000001 # step size for Hamiltoniam dynamics
 #h = 0.0005
 
 # params for gradients
-d_theta = 0.001  # step size for gradient estimate
+d_theta = 0.01  # step size for gradient estimate
 S       = 5
 grad_params = {}
 
@@ -29,7 +29,7 @@ upper_bounds = None #np.array([np.inf])
 
 if __name__ == "__main__":
   # pp.close('all')
-  nnparams = [28*28, 5, 28*28]
+  nnparams = [28*28, 100, 28*28]
   problem = NeuralNetworkProblem( NeuralNetwork(nnparams), load_mnist() )
   # Cheat by loading previous training NN
   # problem.nn.load('latest_epoch.json')
@@ -42,16 +42,17 @@ if __name__ == "__main__":
   params["d_theta"] = d_theta
   params["eta"]     = eta
   params["C"]       = C
-  params["batch_size"] = 1000
+  params["batch_size"] = 5
   params["verbose_rate"] = verbose_rate
   params["grad_params"]  = {"logs":{"true":[],"true_abc":[],"2side_keps":[],"2side_sl":[]},\
                             "record_2side_sl_grad":False, "record_2side_keps_grad":False,"record_true_abc_grad":False,"record_true_grad":False,
-                            "2side_keps": {"R": 10},"percent_to_change":0.25}
+                            "2side_keps": {"R": 5},"percent_to_change":0.25}
   params["lower_bounds"] = lower_bounds
   params["upper_bounds"] = upper_bounds
   params["keep_x"]       = keep_x
 
-  theta0 = problem.nn.weights
+  #theta0 = problem.nn.weights
+  theta0 = problem.flatten(problem.nn.biases, problem.nn.weights)
   x0     = None
 
   # initialize the simulator
@@ -64,9 +65,9 @@ if __name__ == "__main__":
   print "nn", nnparams
   print params
   # run algorithm
-  # outs = run_thermostats( problem, params, theta0, x0 )
+  outs = run_thermostats( problem, params, theta0, x0 )
   #outs = run_sghmc( problem, params, theta0, x0 )
-  outs = run_sgld( problem, params, theta0, x0 )
+  #outs = run_sgld( problem, params, theta0, x0 )
   #outs = run_sghmc( problem, params, theta0, x0 )
   #outs = run_mcmc( problem, params, theta0, x0 )
 

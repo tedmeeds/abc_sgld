8b347af537332eb24cb6d6afd2fa9f4f6a770a96
LeendersR <robert.leenders@student.uva.nl>
2015-03-03 23:31:44 +0100

    added extract option

--- code/logistic_regression/plots.py
+++ code/logistic_regression/plots.py
@@ -68,8 +68,37 @@ def plot_PCA(filename, random_proj=None):
     # plt.ylim((-345, -350))
     # plt.show()
 
+def extract():
+    file = open('LR2.json', "r")
+    LR_data = json.load(file)
+    file.close()
+    MAP = np.array(LR_data['weights'][-1]).flatten()
+    random_proj = np.random.randn( len(MAP),2 )
+    MAP_proj = np.dot(MAP, random_proj)
+    extracted_data = {'MAP_proj': MAP_proj, 'MAP': MAP}
+    shortcuts = ['true_sgld', 'true_thermo', 'spsa_sgld', 'spsa_thermo']
+    filenames = ['sampling-sgld-truegradient-eta=0.01-C=100.json',
+                 'sampling-thermo-truegradient-eta=0.01-C=20.json',
+                 'sampling-sgld-eta=0.01-C=100.json',
+                 'sampling-thermo-eta=0.01-C=20.json']
+    for i in range(len(filenames)):
+        file = open(filenames[i], "r")
+        data = json.load(file)
+        file.close()
+        W = np.array([np.array(w).flatten() for w in data["weights"]])
+        W_proj = np.dot(W, random_proj)
+        W_proj -= MAP_proj
+        extracted_data[shortcuts[i]] = W_proj
+
+    return extracted_data
+
+
 
 if __name__ == '__main__':
+    data = extract()
+    print data['MAP']
+    exit()
+
     file = open('LR2.json', "r")
     LR_data = json.load(file)
     file.close()

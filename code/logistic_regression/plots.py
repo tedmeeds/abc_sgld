fix wrong dimension

code/logistic_regression/plots.py
@@ -4,6 +4,8 @@ import json
 from sklearn.decomposition import PCA
 from sklearn import mixture
 
+K = 2
+
 def plot_LL(filename):
     file = open(filename, "r")
     data = json.load(file)
@@ -13,30 +15,44 @@ def plot_LL(filename):
     plt.plot(data['LLs'])
     plt.show()
 
+
+def create_PCA(filename):
+    file = open(filename, "r")
+    data = json.load(file)
+    file.close()
+    W = np.array([np.array(w).flatten() for w in data["weights"]])
+    pca = PCA(n_components=K)
+    pca = pca.fit(W)
+    return pca
+
 def plot_PCA(filename):
     file = open(filename, "r")
     data = json.load(file)
     file.close()
     # W = np.array([np.array(w) for w in data["weights"][-1]])
-    W = np.array([np.array(w).flatten() for w in data["weights"]]).T
-    K = 2
-    pca = PCA(n_components=K)
-    W_pca = pca.fit(W).transform(W)
+    W = np.array([np.array(w).flatten() for w in data["weights"]])
+    # pca = create_PCA('sampling-true-posterior-mcmc.json')
+    pca = create_PCA(filename)
+    W_pca = pca.transform(W)
     clf = mixture.GMM(n_components=K, covariance_type='full')
     clf.fit(W_pca)
-    x = np.linspace(-100, 100)
-    X, Y = np.meshgrid(x, x)
+    x = np.linspace(-2, 20)
+    y = np.linspace(-2, 2)
+    X, Y = np.meshgrid(x, y)
     XX = np.array([X.ravel(), Y.ravel()]).T
     Z = -clf.score_samples(XX)[0]
     Z = Z.reshape(X.shape)
     plt.figure()
-    plt.contour(X, Y, Z)
+    # plt.contour(X, Y, Z)
     plt.scatter(W_pca[:, 0], W_pca[:, 1])
+    # plt.xlim((-100, 100))
+    # plt.ylim((-30, 30))
     plt.show()
 
 
 if __name__ == '__main__':
-    # filename = 'sampling-sgld2.json'
+    filename = 'sampling-sgld.json'
     # filename = 'LR2.json'
-    filename = 'sampling-MCMC.json'
+    # filename = 'sampling-MCMC.json'
+    # filename = 'sampling-true-posterior-mcmc2.json'
     plot_PCA(filename)

code/logistic_regression/working_code.py
@@ -47,7 +47,7 @@ def get_omega(problem, batch_size):
     #print "avg abs W :", np.mean( np.abs( problem.lr.W))
     get_omega.test_errors.append(error)
     get_omega.avg_test_errors.append(avg_error)
-    get_omega.weights.append(problem.lr.W)
+    # get_omega.weights.append(problem.lr.W)
     get_omega.LLs.append(LL)
 
     data = {
@@ -57,14 +57,14 @@ def get_omega(problem, batch_size):
       'LLs': [ll.tolist() for ll in get_omega.LLs]
     }
 
-    file = open("sampling-sgld3.json", "w")
+    file = open("sampling-sgld2.json", "w")
     json.dump(data, file)
     file.close()
     print 'saved to file'
 
   return mini_batches.pop()
 get_omega.mini_batches = []
-get_omega.counter = 0
+get_omega.counter = 1
 get_omega.average_counter = 0
 get_omega.y_test_avg = 0
 get_omega.Ytest_avg = 0
@@ -184,7 +184,8 @@ def run_mcmc( problem, params, theta, x = None ):
     OMEGAS.append(omega)
 
 
-    if np.mod(t+1,verbose_rate)==0:
+    if np.mod(t+1, verbose_rate) == 0:
+      get_omega.weights.append(theta)
       print "t = %04d    loglik = %3.3f    theta = %3.3f    x = %3.3f"%(t+1,loglike_x,theta[0],x[0][0])
 
   outputs["THETA"] = np.squeeze(np.array( THETAS))
@@ -293,6 +294,7 @@ def run_sgld( problem, params, theta, x = None ):
 
 
     if np.mod(t+1,verbose_rate)==0:
+      get_omega.weights.append(theta)
       if keep_x:
         print "t = %04d    loglik = %3.3f    theta0 = %3.3f    x0 = %3.3f"%(t+1,loglike_x,theta[0],x[0][0])
       else:
@@ -401,6 +403,7 @@ def run_sghmc( problem, params, theta, x = None ):
 
 
     if np.mod(t+1,verbose_rate)==0:
+      get_omega.weights.append(theta)
       if keep_x:
         print "t = %04d    loglik = %3.3f    theta0 = %3.3f    x0 = %3.3f"%(t+1,loglike_x,theta[0],x[0])
       else:
@@ -514,6 +517,7 @@ def run_thermostats( problem, params, theta, x = None ):
 
 
     if np.mod(t+1,verbose_rate)==0:
+      get_omega.weights.append(theta)
       if keep_x:
         print "t = %04d    loglik = %3.3f    theta0 = %3.3f    x0 = %3.3f"%(t+1,loglike_x,theta[0],x[0][0])
       else: